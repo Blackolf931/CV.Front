4239800ff609285d16e6c1a7e0769ad83a38b419
Ilya Safonov <illia220011@gmail.com>
2022-07-18 12:47:29 +0300

    Added an empty project.

---
+++ CV/CV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

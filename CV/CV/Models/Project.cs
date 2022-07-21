be1f501e4cf12df6722fb6d5e573a64695f81b69
AngryKirill <mister.lolich@mail.ru>
2022-07-21 16:08:20 +0300

    Add project model

---
+++ CV/CV/Models/Project.cs
@@ -0,0 +1,14 @@
+﻿namespace CV.Models
+{
+    public class Project
+    {
+#nullable disable
+        public string Name { get; set; }
+        public DateTime StartProject { get; set; }
+        public string Description { get; set; }
+        public string Environment { get; set; }
+#nullable enable
+
+        public DateTime? EndProject { get; set; }
+    }
+}

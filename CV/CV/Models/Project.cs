e07e3faae6e33c2e200cf733f69652096fbce62f
AngryKirill <mister.lolich@mail.ru>
2022-07-21 16:23:19 +0300

    Change to record

--- CV/CV/Models/Project.cs
+++ CV/CV/Models/Project.cs
@@ -1,14 +1,6 @@
 ﻿namespace CV.Models
 {
-    public class Project
-    {
-#nullable disable
-        public string Name { get; set; }
-        public DateTime StartProject { get; set; }
-        public string Description { get; set; }
-        public string Environment { get; set; }
-#nullable enable
-
-        public DateTime? EndProject { get; set; }
-    }
+    public record class Project(int Id, string Name, 
+        DateTime StartProject, string Description, 
+        string Environment, DateTime? EndProject);
 }

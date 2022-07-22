48388f620df1fe2f65316e872f40dcf65597a4dd
Ilya Safonov <illia220011@gmail.com>
2022-07-22 12:00:28 +0300

    Added api uri constants.

---
+++ CV/CV/Constants/ApiUri.cs
@@ -0,0 +1,8 @@
+﻿namespace CV.Constants
+{
+    public static class ApiUri
+    {
+        public static string Employee = "https://localhost:7011/Employee";
+        public static string Project = "https://localhost:7011/Project";
+    }
+}
